397d1fda684b2ed950a47f502be37f7224391398
vishnu244 <vishnunali244@gmail.com>
2022-05-17 17:36:59 +0530

    [Vishnu]- initial commit

---
+++ MoodAnalyzer/AnalyseMood.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class AnalyseMood
+    {
+        public string AnalyseMoodMethod(string message)
+        {
+            string message1 = "Sad";
+            if (message.ToLower().Contains(message1.ToLower()))
+            {
+                return message1;
+            }
+            else
+            {
+                return "Happy";
+            }
+
+        }
+    }
+}
---
+++ MoodAnalyzer/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MoodAnalyzer;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Mood Analyser!");
+AnalyseMood a1 = new();
+string mood = a1.AnalyseMoodMethod("I am in sad mood");
+Console.WriteLine(mood);
