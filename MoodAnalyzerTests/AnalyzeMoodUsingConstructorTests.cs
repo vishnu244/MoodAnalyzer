9fadfa461af8089ae5afd52a06bcd505a99d4900
vishnu244 <vishnunali244@gmail.com>
2022-05-18 12:27:13 +0530

    [Vishnu]-Using default&parameterized constructor TestCases

---
+++ MoodAnalyzerTests/AnalyzeMoodUsingConstructorTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer.Tests
+{
+    [TestClass()]
+    public class AnalyzeMoodUsingConstructorTests
+    {
+        [TestMethod()]
+        public void AnalyzeMoodUsingConstructorTest()
+        {
+            AnalyzeMoodUsingConstructor A1 = new AnalyzeMoodUsingConstructor();
+            string mood1 = A1.AnalyseMoodMethod();
+            Assert.AreEqual("SAD", mood1);
+        }
+        
+    }
+}
